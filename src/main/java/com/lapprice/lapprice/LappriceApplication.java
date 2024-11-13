b2bcb5d3461cbfdbbb3e2032eb084c5cf01f52fc
s-hwan <tmdghks507@daum.net>
2024-11-13 13:34:59 +0900

    init commit

---
+++ src/main/java/com/lapprice/lapprice/LappriceApplication.java
@@ -0,0 +1,13 @@
+package com.lapprice.lapprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LappriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LappriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lapprice/lapprice/LappriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapprice.lapprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LappriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
